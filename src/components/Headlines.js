6ab3eec0167ef274b4133d23c1787c349342a9fe
Jon Stump <jmstump@gmail.com>
2021-05-11 09:45:45 -0700

    add render and API call to Headlines component
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: David Couch <dcouch440@gmail.com>

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -10,8 +10,49 @@ class Headlines extends React.Component {
     };
   }
 
+makeApiCall = () => {
+  fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+  .then(response => response.json())
+  .then(
+    (jsonifiedResponse) => {
+      this.setState({
+        isLoaded: true,
+        headlines: jsonifiedResponse.results
+      });
+    })
+  .catch((error) => {
+    this.setState({
+      isLoaded: true,
+      error
+    });
+  });
+}
+
+componentDidMount() {
+  this.makeApiCall()
+}
+
   render() {
-    ...
+    const {error, isLoaded, headlines } = this.state;
+    if (error) {
+      return <>Error: {error.message}</>;
+    } else if (!isLoaded) {
+      return <>Loading...</>;
+    } else {
+      return (
+        <>
+          <h1>Headlines</h1>
+          <ul>
+            {headlines.map((headlines, index) =>
+            <li key={index}>
+              <h3>{headlines.title}</h3>
+              <p>{headlines.abstract}</p>
+            </li>
+          )}
+          </ul>
+        </>
+      );
+    }
   }
 }
 
